cde4ac7638b8b528769c2b65cdb3fce4d81f83bf
ezkiro <ezkiro@gmail.com>
2016-12-06 00:03:39 +0900

    make new project

---
+++ src/main/java/com/toyfactory/pcb/SgpcbserviceApplication.java
@@ -0,0 +1,12 @@
+package com.toyfactory.pcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpcbserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpcbserviceApplication.class, args);
+	}
+}
