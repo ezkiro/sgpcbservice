f7c2d1081f924742abf5b68a879e5fdc7c100314
ezkiro <ezkiro@gmail.com>
2017-01-23 00:35:19 +0900

    add BatchController

---
+++ src/main/java/com/toyfactory/pcb/api/BatchController.java
@@ -0,0 +1,51 @@
+package com.toyfactory.pcb.api;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.toyfactory.pcb.service.GamePatchService;
+
+@RestController
+@RequestMapping("/batch")
+public class BatchController {
+	private static final Logger logger = LoggerFactory.getLogger(BatchController.class);
+	
+	private static final String AUTH_KEY = "!b@tch9ey!";
+	
+	@Autowired
+	private GamePatchService gamePatchService;	
+	
+    @PostMapping("/gamepatchlog")
+    public String buildGamePatchLog(@RequestParam(value="auth_key", required = true) String authKey) {
+    	
+    	if (logger.isDebugEnabled()) {
+    		logger.debug("batch gamepatchlog: auth_key:" + authKey);
+    	}
+    	
+    	if (!AUTH_KEY.equals(authKey)) {
+    		
+    		return "fail: no auth key!";
+    	}
+    	
+		//reference http://tutorials.jenkov.com/java-util-concurrent/executorservice.html
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+		executorService.execute(new Runnable() {
+		    public void run() {
+				gamePatchService.excuteGamePatchAnalysisBatch();
+		    }
+		});
+		
+		executorService.shutdown();
+				
+		return "OK";
+    }	
+}
